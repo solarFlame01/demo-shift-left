c153ed4c9738903ec932736e29f879b36fe8204b
Giuseppe De Marzo <Gdemarzo94@gmail.com>
2026-02-16 14:35:39 +0100

    Initial app: Simple Todo List with React

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>My Todo List</h1>
+      <div>
+        <input
+          className="new-todo"
+          type="text"
+          placeholder="What needs to be done?"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' && addTodo()}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
